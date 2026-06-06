7691223e605c27d9455460333d12becfd1ecbd24
Jonatha Vianna <jonatha.vianna@prodepa.pa.gov.br>
2026-06-05 23:41:51 -0300

    feat: estruturando MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,15 +4,14 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> Portifólio Jonatha Vianna </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header> Dev Jonatha </q-item-label>
 
         <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
@@ -30,46 +29,34 @@ import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink
 
 const linksList: EssentialLinkProps[] = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
+    title: 'Sobre Mim',
+    caption: 'Sobre Mim',
+    icon: 'person',
+    link: '',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
+    title: 'Experiência Profissional',
+    caption: 'Experiência Profissional',
+    icon: 'work',
+    link: '',
+  },
+  {
+    title: 'Projetos',
+    caption: 'Projetos',
     icon: 'code',
-    link: 'https://github.com/quasarframework',
+    link: '',
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
+    title: 'Stack Tecnológica',
+    caption: 'Stack Tecnológica',
+    icon: 'build',
+    link: '',
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
+    title: 'Contato',
+    caption: 'Contato',
+    icon: 'contact_mail',
+    link: '',
   },
 ];
 
